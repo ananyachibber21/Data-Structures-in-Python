52260eaf037ba8ea39011ee8f9c0259553485b32
ananyachibber21 <ananyaunique21@gmail.com>
2024-01-10 19:29:51 +0530

    Added Merge Sort

---
+++ Java/Arrays/MergeSort.java
@@ -0,0 +1,81 @@
+class Merge {
+    private int[] arr;
+
+    public Merge(int[] arr) {
+        this.arr = arr;
+    }
+
+    public void merge(int[] arr, int left, int middle, int right) {
+        int n1 = middle - left + 1;
+        int n2 = right - middle;
+
+        int[] leftArray = new int[n1];
+        int[] rightArray = new int[n2];
+
+        for (int i = 0; i < n1; ++i) {
+            leftArray[i] = arr[left + i];
+        }
+        for (int j = 0; j < n2; ++j) {
+            rightArray[j] = arr[middle + 1 + j];
+        }
+
+        int i = 0, j = 0;
+        int k = left;
+        while (i < n1 && j < n2) {
+            if (leftArray[i] <= rightArray[j]) {
+                arr[k] = leftArray[i];
+                i++;
+            } else {
+                arr[k] = rightArray[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1) {
+            arr[k] = leftArray[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            arr[k] = rightArray[j];
+            j++;
+            k++;
+        }
+    }
+
+    public void mergeSort(int[] arr, int left, int right) {
+        if (left < right) {
+            int middle = (left + right) / 2;
+
+            mergeSort(arr, left, middle);
+            mergeSort(arr, middle + 1, right);
+
+            merge(arr, left, middle, right);
+        }
+    }
+
+    public void sorted_arr() {
+        int n = arr.length;
+        mergeSort(arr, 0, n - 1);
+    }
+
+    public void print_arr() {
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+}
+
+public class MergeSort {
+    public static void main(String[] args) {
+        int[] arr = {6, 2, 9, 0, 3, 5, 1};
+        Merge m = new Merge(arr);
+        m.sorted_arr(); // Sort the array
+        System.out.println("The sorted array: ");
+        m.print_arr(); // Print the sorted array
+    }
+}
